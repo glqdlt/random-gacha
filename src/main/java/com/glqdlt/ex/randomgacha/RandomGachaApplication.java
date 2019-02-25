22fd48430be987f8ebb58f3a194b1819349b48af
jhun <jhun@nm-4ones.com>
2019-02-25 10:51:20 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/randomgacha/RandomGachaApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.randomgacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomGachaApplication.class, args);
+    }
+
+}
